206b2e99081e22f631f20c92438472f84788e3bd
Kai Kewley <kaikewley@gmail.com>
2017-11-29 09:36:15 +0000

    Percentage instead of decimal number chance in data generation.

--- src/main/java/org/snomed/heathanalytics/ingestion/exampledata/ExampleDataGenerator.java
+++ src/main/java/org/snomed/heathanalytics/ingestion/exampledata/ExampleDataGenerator.java
@@ -58,7 +58,7 @@ public class ExampleDataGenerator implements HealthDataIngestionSource {
 		patient.setDob(dateOfBirthFromAge(age));
 
 		//  50% of patients are Male.
-		if (chance(0.5f)) {
+		if (chancePercent(50)) {
 			patient.setGender(Gender.MALE);
 		} else {
 			patient.setGender(Gender.FEMALE);
@@ -72,24 +72,24 @@ public class ExampleDataGenerator implements HealthDataIngestionSource {
 		date.add(Calendar.DAY_OF_YEAR, ThreadLocalRandom.current().nextInt(30, 30 * 6));
 
 		// 10% of patients have diabetes.
-		if (chance(0.1f)) {
+		if (chancePercent(10)) {
 			patient.addEncounter(new ClinicalEncounter(date.getTime(), ClinicalEncounterType.FINDING, concepts.selectRandomChildOf("420868002")));// Disorder due to type 1 diabetes mellitus
 
 			// After 1 - 2 months
 			date.add(Calendar.DAY_OF_YEAR, ThreadLocalRandom.current().nextInt(30, 30 * 2));
 
 			// 7% of the diabetic patients also have Peripheral Neuropathy.
-			if (chance(0.07f)) {
+			if (chancePercent(7)) {
 				patient.addEncounter(new ClinicalEncounter(date.getTime(), ClinicalEncounterType.FINDING, concepts.selectRandomChildOf("302226006")));// Peripheral Neuropathy
 			}
 
 			// 10% of the diabetic patients have a Myocardial Infarction.
-			if (chance(0.1f)) {
+			if (chancePercent(10)) {
 				patient.addEncounter(new ClinicalEncounter(date.getTime(), ClinicalEncounterType.FINDING, concepts.selectRandomChildOf("22298006")));// Myocardial Infarction
 			}
 		} else {
 			// 1% of the non-diabetic patients have Peripheral Neuropathy.
-			if (chance(0.01f)) {
+			if (chancePercent(1)) {
 				patient.addEncounter(new ClinicalEncounter(date.getTime(), ClinicalEncounterType.FINDING, concepts.selectRandomChildOf("302226006")));// Peripheral Neuropathy
 			}
 		}
@@ -98,11 +98,11 @@ public class ExampleDataGenerator implements HealthDataIngestionSource {
 		date.add(Calendar.DAY_OF_YEAR, ThreadLocalRandom.current().nextInt(30, 30 * 3));
 
 		// 30 % of patients over 40 years old have hypertension.
-		if (age > 40 && chance(0.3f)) {
+		if (age > 40 && chancePercent(30)) {
 			patient.addEncounter(new ClinicalEncounter(date.getTime(), ClinicalEncounterType.FINDING, concepts.selectRandomChildOf("38341003")));// Hypertension
 
 			// 50% of patients over 40 with hypertension are prescribed an Antiplatelet agent
-			if (chance(0.5f)) {
+			if (chancePercent(50)) {
 				// Prescribed an Antiplatelet agent
 				patient.addEncounter(new ClinicalEncounter(date.getTime(), ClinicalEncounterType.MEDICATION, concepts.selectRandomChildOf("108972005")));// Antiplatelet agent (product)
 
@@ -110,7 +110,7 @@ public class ExampleDataGenerator implements HealthDataIngestionSource {
 				date.add(Calendar.DAY_OF_YEAR, ThreadLocalRandom.current().nextInt(30, 30 * 6));
 
 				// 2% of patients with hypertension who have been prescribed an Antiplatelet agent have a Myocardial Infarction.
-				if (chance(0.02f)) {
+				if (chancePercent(2)) {
 					patient.addEncounter(new ClinicalEncounter(date.getTime(), ClinicalEncounterType.FINDING, concepts.selectRandomChildOf("22298006")));// Myocardial Infarction
 				}
 			} else {
@@ -120,7 +120,7 @@ public class ExampleDataGenerator implements HealthDataIngestionSource {
 				date.add(Calendar.DAY_OF_YEAR, ThreadLocalRandom.current().nextInt(30, 30 * 6));
 
 				// 8% of patients with hypertension who have NOT been prescribed an Antiplatelet agent have a Myocardial Infarction.
-				if (chance(0.08f)) {
+				if (chancePercent(8)) {
 					patient.addEncounter(new ClinicalEncounter(date.getTime(), ClinicalEncounterType.FINDING, concepts.selectRandomChildOf("22298006")));// Myocardial Infarction
 				}
 			}
@@ -130,19 +130,19 @@ public class ExampleDataGenerator implements HealthDataIngestionSource {
 		date.add(Calendar.DAY_OF_YEAR, ThreadLocalRandom.current().nextInt(30, 30 * 2));
 
 		// 5% of all patients over 55 years old have Myocardial Infarction.
-		if (age > 55 && chance(0.05f)) {
+		if (age > 55 && chancePercent(5)) {
 			patient.addEncounter(new ClinicalEncounter(date.getTime(), ClinicalEncounterType.FINDING, concepts.selectRandomChildOf("22298006")));// Myocardial Infarction
 		}
 
 		//
 		// Begin section RA and COPD ------------------------
 		//
-		if (age > 15 && chance(0.06f)) {// Patients with both RA and COPD very small
+		if (age > 15 && chancePercent(6)) {// Patients with both RA and COPD very small
 			patient.addEncounter(new ClinicalEncounter(date.getTime(), ClinicalEncounterType.FINDING, concepts.selectRandomChildOf("69896004")));// Rheumatoid Arthritis
 			patient.addEncounter(new ClinicalEncounter(date.getTime(), ClinicalEncounterType.FINDING, concepts.selectRandomChildOf("13645005")));// COPD
 
 			// 50% of patients over 15 with Rheumatoid Arthritis and COPD are prescribed an Anti-TNF
-			if (chance(0.5f)) {
+			if (chancePercent(50)) {
 				// Prescribed an Anti-TNF agent
 				patient.addEncounter(new ClinicalEncounter(date.getTime(), ClinicalEncounterType.MEDICATION, concepts.selectRandomChildOf("416897008")));// Anti-TNF agent (product)
 
@@ -150,7 +150,7 @@ public class ExampleDataGenerator implements HealthDataIngestionSource {
 				date.add(Calendar.DAY_OF_YEAR, ThreadLocalRandom.current().nextInt(30, 30 * 6));
 
 				// 10% of patients with RA and COPD who have been prescribed an AntiTNF agent have a Lung Infection.
-				if (chance(0.1f)) {
+				if (chancePercent(10)) {
 					patient.addEncounter(new ClinicalEncounter(date.getTime(), ClinicalEncounterType.FINDING, concepts.selectRandomChildOf("53084003")));// Bacterial Lung Infection
 				}
 			} else { // other 50%
@@ -160,7 +160,7 @@ public class ExampleDataGenerator implements HealthDataIngestionSource {
 				date.add(Calendar.DAY_OF_YEAR, ThreadLocalRandom.current().nextInt(30, 30 * 6));
 
 				// 2% of patients with RA and COPD who have NOT been prescribed an Anti-TNF agent have a Bacterial Lung Infection.
-				if (chance(0.02f)) {
+				if (chancePercent(2)) {
 					patient.addEncounter(new ClinicalEncounter(date.getTime(), ClinicalEncounterType.FINDING, concepts.selectRandomChildOf("53084003")));// Bacterial Lung Infection
 				}
 			}
@@ -168,10 +168,10 @@ public class ExampleDataGenerator implements HealthDataIngestionSource {
 		// End of section of RA and COPD ---------------------
 
 		// Begin section RA only  ----------------------------
-		if (age > 15 && chance(0.03f)) {//Patients with RA only
+		if (age > 15 && chancePercent(3)) {//Patients with RA only
 			patient.addEncounter(new ClinicalEncounter(date.getTime(), ClinicalEncounterType.FINDING, concepts.selectRandomChildOf("69896004")));// Rheumatoid Arthritis
 
-			if (chance(0.5f)) {// about half of them get TNF inhibitor
+			if (chancePercent(50)) {// about half of them get TNF inhibitor
 				// Prescribed an Anti-TNF agent
 				patient.addEncounter(new ClinicalEncounter(date.getTime(), ClinicalEncounterType.MEDICATION, concepts.selectRandomChildOf("416897008")));// Anti-TNF agent (product)
 
@@ -180,7 +180,7 @@ public class ExampleDataGenerator implements HealthDataIngestionSource {
 				date.add(Calendar.DAY_OF_YEAR, ThreadLocalRandom.current().nextInt(30, 30 * 6));
 
 				// 4% of patients with RA only who have been prescribed an AntiTNF agent have a Lung Infection.
-				if (chance(0.04f)) {
+				if (chancePercent(4)) {
 					patient.addEncounter(new ClinicalEncounter(date.getTime(), ClinicalEncounterType.FINDING, concepts.selectRandomChildOf("53084003")));// Bacterial Lung Infection
 				}
 			} else {
@@ -190,7 +190,7 @@ public class ExampleDataGenerator implements HealthDataIngestionSource {
 				date.add(Calendar.DAY_OF_YEAR, ThreadLocalRandom.current().nextInt(30, 30 * 6));
 
 				// 0.5% of patients with who have NOT been prescribed an Anti-TNF agent have a Bacterial Lung Infection.
-				if (chance(0.005f)) {
+				if (chancePercent(0.5f)) {
 					patient.addEncounter(new ClinicalEncounter(date.getTime(), ClinicalEncounterType.FINDING, concepts.selectRandomChildOf("53084003")));// Bacterial Lung Infection
 				}
 			}
@@ -199,7 +199,7 @@ public class ExampleDataGenerator implements HealthDataIngestionSource {
 
 		// Begin section of COPD only ---------------------
 		// 6% with COPD
-		if (age > 15 && chance(0.06f)) {//Patients with COPD only
+		if (age > 15 && chancePercent(6)) {//Patients with COPD only
 			patient.addEncounter(new ClinicalEncounter(date.getTime(), ClinicalEncounterType.FINDING, concepts.selectRandomChildOf("13645005")));// COPD
 
 			// None with COPD alone would get anti-TNF so remove those lines
@@ -208,7 +208,7 @@ public class ExampleDataGenerator implements HealthDataIngestionSource {
 			date.add(Calendar.DAY_OF_YEAR, ThreadLocalRandom.current().nextInt(30, 30 * 6));
 
 			// 2% of p	atients with COPD only have a Lung Infection.
-			if (chance(0.02f)) {
+			if (chancePercent(2)) {
 				patient.addEncounter(new ClinicalEncounter(date.getTime(), ClinicalEncounterType.FINDING, concepts.selectRandomChildOf("53084003")));// Bacterial Lung Infection
 			}
 		}
@@ -216,7 +216,12 @@ public class ExampleDataGenerator implements HealthDataIngestionSource {
 		healthDataOutputStream.createPatient(patient);
 	}
 
+	private boolean chancePercent(float probability) {
+		return chance(probability / 100);
+	}
+
 	private boolean chance(float probability) {
+		System.out.println(probability);
 		return probability >= Math.random();
 	}
 

c405c3a4263f4391652b952460c0250e6874b386
Kai Kewley <kaikewley@gmail.com>
2019-01-14 13:37:12 +0000

    Fix #5 Gaurd against concepts which are missing or with no descendants.
    Since the July 2018 International Release concept 350468007 | Direct acting anticoagulant (product) | has had no active descendants which has been causing generation of the demonstration population to fail. This fix uses the concept quoted in the data generation rules if it has no decendants after logging a warning.

--- src/main/java/org/snomed/heathanalytics/ingestion/exampledata/ExampleConceptService.java
+++ src/main/java/org/snomed/heathanalytics/ingestion/exampledata/ExampleConceptService.java
@@ -1,18 +1,26 @@
 package org.snomed.heathanalytics.ingestion.exampledata;
 
 import org.ihtsdo.otf.sqs.service.SnomedQueryService;
+import org.ihtsdo.otf.sqs.service.dto.ConceptResult;
 import org.ihtsdo.otf.sqs.service.exception.ServiceException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
+
+import static java.lang.Long.parseLong;
 
 public class ExampleConceptService {
 
 	private SnomedQueryService snomedQueryService;
 
 	private final Map<Long, List<Long>> conceptDescendantMap;
+
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 
 	public ExampleConceptService(SnomedQueryService snomedQueryService) {
 		this.snomedQueryService = snomedQueryService;
@@ -22,12 +30,12 @@ public class ExampleConceptService {
 	Long selectRandomChildOf(String conceptId) throws ServiceException {
 		List<Long> descendants;
 		try {
-			descendants = getDescendants(Long.parseLong(conceptId));
+			descendants = getDescendants(parseLong(conceptId));
 		} catch (ServiceException e) {
 			throw new ServiceException("Failed to fetch descendants of " + conceptId, e);
 		}
 		if (descendants.isEmpty()) {
-			return null;
+			throw new ServiceException("Concept " + conceptId + " could not be found.");
 		}
 		return descendants.get(ThreadLocalRandom.current().nextInt(0, descendants.size()));
 	}
@@ -36,8 +44,17 @@ public class ExampleConceptService {
 		if (!conceptDescendantMap.containsKey(conceptId)) {
 			synchronized (conceptDescendantMap) {
 				if (!conceptDescendantMap.containsKey(conceptId)) {
+					List<Long> descendants = snomedQueryService.eclQueryReturnConceptIdentifiers("<" + conceptId, 0, 1000).getConceptIds();
+					if (descendants.isEmpty()) {
+						ConceptResult conceptResult = snomedQueryService.retrieveConcept(conceptId.toString());
+						if (conceptResult == null) {
+							throw new ServiceException("Concept " + conceptId + " could not be found.");
+						}
+						logger.warn("Concept " + conceptId + " has no descendants, using the concept itself.");
+						descendants = Collections.singletonList(conceptId);
+					}
 					conceptDescendantMap.put(conceptId,
-							snomedQueryService.eclQueryReturnConceptIdentifiers("<" + conceptId, 0, 1000).getConceptIds());
+							descendants);
 				}
 			}
 		}

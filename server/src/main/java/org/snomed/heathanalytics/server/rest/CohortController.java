b029b73b6dc0ade84a3697613106e8ddd65da605
Kai Kewley <kaikewley@gmail.com>
2020-08-16 21:47:30 +0100

    Sort patient encounters by date when returned via API.

--- server/src/main/java/org/snomed/heathanalytics/server/rest/CohortController.java
+++ server/src/main/java/org/snomed/heathanalytics/server/rest/CohortController.java
@@ -2,6 +2,7 @@ package org.snomed.heathanalytics.server.rest;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.snomed.heathanalytics.model.ClinicalEncounter;
 import org.snomed.heathanalytics.server.model.CohortCriteria;
 import org.snomed.heathanalytics.model.Patient;
 import org.snomed.heathanalytics.server.service.QueryService;
@@ -9,6 +10,9 @@ import org.snomed.heathanalytics.server.service.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Comparator;
+import java.util.TreeSet;
 
 @RestController
 @Api(tags = "Patients", description = "-")
@@ -24,7 +28,13 @@ public class CohortController {
 	public Page<Patient> runCohortSelection(@RequestBody CohortCriteria cohortCriteria,
 											@RequestParam(required = false, defaultValue = "0") int page,
 											@RequestParam(required = false, defaultValue = "100") int size) throws ServiceException {
-		return queryService.fetchCohort(cohortCriteria, page, size);
+		Page<Patient> patients = queryService.fetchCohort(cohortCriteria, page, size);
+		for (Patient patient : patients.getContent()) {
+			TreeSet<ClinicalEncounter> encounters = new TreeSet<>(Comparator.comparing(ClinicalEncounter::getDate).thenComparing(ClinicalEncounter::getConceptId));
+			encounters.addAll(patient.getEncounters());
+			patient.setEncounters(encounters);
+		}
+		return patients;
 	}
 
 }
